22d2024a906aeff56537a172b2dabcbe3c10e5c6
Alisson Nunes <Alynva@users.noreply.github.com>
2016-11-10 11:22:50 -0200

    Create script.objExe.js

---
+++ js/script.objExe.js
@@ -0,0 +1,12 @@
+/* Objetos que são recebidos do PHP */
+function Exercicio() {
+	this.name;
+	this.img;
+	this.style;
+	this.group;
+	this.equipment;
+	this.steps;
+	this.reps;
+}
+
+//var bd = bd || [];
